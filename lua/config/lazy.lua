081f0a1de25a6349fcb54f8e4fbd1bc788453e77
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2024-02-21 15:27:05 +0100

    disable go plugin and update deps

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,8 +16,8 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     --
+    -- { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.rust" },
-    { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
 
