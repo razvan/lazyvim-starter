8e9d574b942a714310341f766fd22ce87cb1154c
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2024-04-18 15:04:53 +0200

    colorscheme: catppuccin

--- init.lua
+++ init.lua
@@ -1,8 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
--- vim.cmd.colorscheme("slate")
-
 vim.api.nvim_set_option("wrap", true)
 vim.api.nvim_set_option("linebreak", true)
 vim.api.nvim_set_option("clipboard", "unnamedplus")
