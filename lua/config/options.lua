00c97db02e0c9fc47fb24485e633d8d7e63b27dd
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2024-06-05 20:52:18 +0200

    fix: do not use deprecated nvim apis

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,21 +3,13 @@
 -- Add any additional options here
 vim.opt.spelllang = { "en", "de" }
 
-vim.api.nvim_set_option("wrap", true)
-vim.api.nvim_set_option("linebreak", true)
-vim.api.nvim_set_option("clipboard", "unnamedplus")
-
--- This is not preserved across file modes apparently.
--- vim.api.nvim_set_option("colorcolumn", "80")
-vim.opt.colorcolumn = "80"
+vim.api.nvim_set_option_value("wrap", true, { scope = "global" })
+vim.api.nvim_set_option_value("linebreak", true, { scope = "global" })
+vim.api.nvim_set_option_value("clipboard", "unnamedplus", { scope = "global" })
+vim.api.nvim_set_option_value("colorcolumn", "120", { scope = "global" })
 
 -- To disable format on save for a buffer or globally:
 --   vim.b.autoformat=false
 --   vim.g.autoformat=false
 -- Disable annoying auto parens
 vim.g.minipairs_disable = true
-
--- LSP Server to use for Python.
--- Set to "basedpyright" to use basedpyright instead of pyright.
--- vim.g.lazyvim_python_lsp = "basedpyright"
--- vim.g.lazyvim_python_lsp = "pyright"
