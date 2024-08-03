21402a075b8bf6ad1c775c25aaf2c68e914c75e9
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2024-08-03 20:20:57 +0200

    fix(diffview): comment out broken keys

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -2,5 +2,15 @@ return {
   -- See: https://github.com/sindrets/diffview.nvim
   {
     "sindrets/diffview.nvim",
+    -- keys = {
+    --   {
+    --     "<leader>",
+    --     function()
+    --       local view = require("diffview").M.completers.DiffViewFileHistory
+    --       view("%")
+    --     end,
+    --     desc = "Open DiffView",
+    --   },
+    -- },
   },
 }
