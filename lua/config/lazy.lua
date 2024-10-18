9ccccb5c12dd3c8abdb5eeca1c3eb057abbce8e5
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2024-10-18 11:55:56 +0200

    disable plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,12 +20,12 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
-    { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.lang.markdown" },
+    -- { import = "lazyvim.plugins.extras.lang.json" },
+    -- { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.python" },
-    { import = "lazyvim.plugins.extras.coding.copilot" },
+    -- { import = "lazyvim.plugins.extras.coding.copilot" },
 
     { import = "plugins" },
   },
