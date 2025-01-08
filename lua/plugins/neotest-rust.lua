bc0d55158ad0f68c80278e4d751ec101a5e64d34
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2025-01-08 12:00:21 +0100

    fix(neotest): use the same config as astro

--- lua/plugins/neotest-rust.lua
+++ lua/plugins/neotest-rust.lua
@@ -1,44 +1,102 @@
 return {
-  {
-    "rouge8/neotest-rust",
-  },
-  {
-    "nvim-neotest/neotest",
-    dependencies = {
-      "nvim-neotest/nvim-nio",
-      "nvim-lua/plenary.nvim",
-      "antoinemadec/FixCursorHold.nvim",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    opts = {
-      -- Can be a list of adapters like what neotest expects,
-      -- or a list of adapter names,
-      -- or a table of adapter names, mapped to adapter configs.
-      -- The adapter will then be automatically loaded with the config.
-      adapters = {
-        ["neotest-rust"] = {
-          args = { "--nocapture" },
-        },
+  "nvim-neotest/neotest",
+  config = function()
+    -- get neotest namespace (api call creates or returns namespace)
+    local neotest_ns = vim.api.nvim_create_namespace("neotest")
+    vim.diagnostic.config({
+      virtual_text = {
+        format = function(diagnostic)
+          local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+          return message
+        end,
       },
-      -- Example for loading neotest-golang with a custom config
-      -- adapters = {
-      --   ["neotest-golang"] = {
-      --     go_test_args = { "-v", "-race", "-count=1", "-timeout=60s" },
-      --     dap_go_enabled = true,
-      --   },
-      -- },
-      -- keys = {
-      --   {"<leader>t", "", desc = "+test"},
-      --   { "<leader>tt", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Run File (Neotest)" },
-      --   { "<leader>tT", function() require("neotest").run.run(vim.uv.cwd()) end, desc = "Run All Test Files (Neotest)" },
-      --   { "<leader>tr", function() require("neotest").run.run() end, desc = "Run Nearest (Neotest)" },
-      --   { "<leader>tl", function() require("neotest").run.run_last() end, desc = "Run Last (Neotest)" },
-      --   { "<leader>ts", function() require("neotest").summary.toggle() end, desc = "Toggle Summary (Neotest)" },
-      --   { "<leader>to", function() require("neotest").output.open({ enter = true, auto_close = true }) end, desc = "Show Output (Neotest)" },
-      --   { "<leader>tO", function() require("neotest").output_panel.toggle() end, desc = "Toggle Output Panel (Neotest)" },
-      --   { "<leader>tS", function() require("neotest").run.stop() end, desc = "Stop (Neotest)" },
-      --   { "<leader>tw", function() require("neotest").watch.toggle(vim.fn.expand("%")) end, desc = "Toggle Watch (Neotest)" },
-      -- },
+    }, neotest_ns)
+    require("neotest").setup({
+      -- your neotest config here
+      adapters = {
+        require("neotest-rust")({
+          args = { "--no-capture" },
+        }),
+        require("neotest-go"),
+        -- require "neotest-python",
+      },
+    })
+  end,
+  -- ft = { "go", "rust", "python" },
+  ft = { "go", "rust" },
+  dependencies = {
+    "rouge8/neotest-rust",
+    "nvim-neotest/neotest-go",
+    -- "nvim-neotest/neotest-python",
+
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  keys = {
+    { "<leader>T", "", desc = "+test" },
+    {
+      "<leader>Tt",
+      function()
+        require("neotest").run.run(vim.fn.expand("%"))
+      end,
+      desc = "Run File (Neotest)",
+    },
+    {
+      "<leader>TT",
+      function()
+        require("neotest").run.run(vim.uv.cwd())
+      end,
+      desc = "Run All Test Files (Neotest)",
+    },
+    {
+      "<leader>Tr",
+      function()
+        require("neotest").run.run()
+      end,
+      desc = "Run Nearest (Neotest)",
+    },
+    {
+      "<leader>Tl",
+      function()
+        require("neotest").run.run_last()
+      end,
+      desc = "Run Last (Neotest)",
+    },
+    {
+      "<leader>Ts",
+      function()
+        require("neotest").summary.toggle()
+      end,
+      desc = "Toggle Summary (Neotest)",
+    },
+    {
+      "<leader>To",
+      function()
+        require("neotest").output.open({ enter = true, auto_close = true })
+      end,
+      desc = "Show Output (Neotest)",
+    },
+    {
+      "<leader>TO",
+      function()
+        require("neotest").output_panel.toggle()
+      end,
+      desc = "Toggle Output Panel (Neotest)",
+    },
+    {
+      "<leader>TS",
+      function()
+        require("neotest").run.stop()
+      end,
+      desc = "Stop (Neotest)",
+    },
+    {
+      "<leader>Tw",
+      function()
+        require("neotest").watch.toggle(vim.fn.expand("%"))
+      end,
+      desc = "Toggle Watch (Neotest)",
     },
   },
 }
